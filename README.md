2060d6893313f5d57d6cf711945beeb46e442f9b
Robert Honsby <rhonsby@gmail.com>
2014-06-09 22:59:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Parakeet
+
+Parakeet is a Twitter client where the insertion of a Twitter username will yield their most recent tweets.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parakeet::Application.config.secret_key_base = '39cb100c17e85de37477fca654d1621e60130d5592fc728679d1c22799e51d685b413cc0a53d30e08765ca15854080b8ef0a2b19a7ddf53801eb8a8d5e873bbd'
